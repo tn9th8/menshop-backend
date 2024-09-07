e57d8e779ce6162bd081352ce96641ffaf7c0015
tn9th8 <tn9th8@gmail.com>
2024-09-07 15:39:43 +0700

    helmet, compression

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // get port from .env
+  // config port
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
+
+  // config security
+  app.use(helmet());
+
+  // config tools
+  app.use(compression())
 
   await app.listen(port);
 }
